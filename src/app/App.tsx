57bd81c47d91c5c2b13dbdd58a4e210c8fa5f68b
NickKoepr <nickkuiper44@gmail.com>
2025-06-22 22:19:23 +0200

    Added language selector.
    Still need to style the selector.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,22 @@ import GuessField from "./components/guess/GuessField";
 import LetterRow from "./components/letterRow/LetterRow";
 import LetterTile from "./components/letterTile/LetterTile";
 import Title from "./components/title/Title";
-import { checkWord, initGame, isFinished } from "../model/Game";
+import {
+  checkWord,
+  initGame,
+  isFinished,
+  setLanguage,
+  type Language,
+} from "../model/Game";
 import StatusMessage from "./components/message/StatusMessage";
 import DefaultButton from "./components/common/button/DefaultButton";
+import DefaultSelector from "./components/common/selector/DefaultSelector";
 
 function App() {
   const [gameState, setGameState] = useState(() => initGame());
   const [isGameFinished, setGameFinished] = useState(false);
   const [errorState, setErrorState] = useState<string | null>(null);
+  const [selectedLanguage, setSelectedLanguage] = useState<Language>("en");
 
   // Function checks the current word and checks if the game is finished.
   // When the word is not found inside the pre-defined word list, an error is
@@ -42,8 +50,19 @@ function App() {
     }
   };
 
+  const changeLanguage = (lang: Language) => {
+    setLanguage(lang);
+    setSelectedLanguage(lang);
+    setGameState(initGame());
+  };
+
   return (
     <>
+      <DefaultSelector
+        items={["en", "nl"]}
+        selected={selectedLanguage}
+        onSelectChange={(lang) => changeLanguage(lang as Language)}
+      />
       <div className="game-container">
         <Title title="Guess the word"></Title>
 
